762dc27111646a8db08d49cb4d638ce3c4a9c6f6
cabumiscool <jdraudales1@gmail.com>
2020-08-14 15:18:21 -0600

    added a new exception

--- src/dependencies/database/database_exceptions.py
+++ src/dependencies/database/database_exceptions.py
@@ -6,3 +6,6 @@ class DatabaseInitError(Exception):
 class DatabaseMissingArguments(Exception):
     """raised when a method of the database is missing arguments"""
 
+
+class DatabaseDuplicateEntry(Exception):
+    """raised when a method of the database failes because an similar entry already exists"""
