7a111a42b1343e94400b1bcf52265e10e7e7df83
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-09-06 02:24:35 +0530

    Making me God. :squerp:

--- src/bot/cogs/bot_checks.py
+++ src/bot/cogs/bot_checks.py
@@ -10,12 +10,12 @@ def check_permission_level(required_level: int = 0):
         bot = ctx.bot
         db: Database = bot.db
         author: discord.Member = ctx.author
-        is_owner: bool = await ctx.bot.is_owner(ctx.author)
+        is_god: bool = await ctx.bot.is_owner(ctx.author) or ctx.author.id in [479487273432514560, 339050854453608459]
         ids = [author.id, *[role.id for role in author.roles]]
         perm = await db.permission_retriever(*ids)
         if perm is None:
             perm = 0
-        if perm > required_level or is_owner:
+        if perm > required_level or is_god:
             return True
         else:
             raise bot_exceptions.NotEnoughPerms(f"{ctx.author} does not have enough permission to run the command")
