c600f786e9eb8994ec184c860ef69772548b1d57
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-12-19 21:05:23 +0530

    Added: QiCommands Cog
    Includes a minor method to convert book_string to `Book` interactively.

---
+++ src/bot/cogs/qi_commnads.py
@@ -0,0 +1,47 @@
+import asyncio
+from typing import Union
+
+from discord.ext import commands
+from discord.ext.commands import Context
+
+from . import bot_checks
+from bot.bot_utils import *
+from dependencies.webnovel.classes import *
+from dependencies.webnovel.utils import book_string_matcher
+from dependencies.database import PgDatabase
+
+
+class QiCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.db: PgDatabase = bot.db
+
+    @commands.command(aliases=['b'])
+    @bot_checks.check_permission_level(2)
+    async def buy(self, ctx: Context, *args) -> Union[SimpleBook, None]:
+        pass
+
+    def __interactive_book_string_to_book(self, ctx: Context, book_string: str, limit: int = 5) -> Union[Book, None]:
+        possible_matches = await book_string_matcher(self.db, book_string, limit)
+
+        if possible_matches is None:
+            return None
+        elif len(possible_matches) == 1:
+            return possible_matches[0][0]
+
+        # TODO: Decide on embed colours
+        description = 'Please select the required book:'
+        embed = generate_embed(f'Book Selection for {book_string}', ctx.author, description=description, color=0)
+        for x in range(len(possible_matches)):
+            book = possible_matches[x][0]
+            score = possible_matches[x][1]
+            name = f"{NUMERIC_EMOTES_LIST[x]} {book.name}"
+            value = f"Score : `{score}`\n" \
+                    f"Abbreviation : `{book.abbreviation}`"
+            embed.add_field(name=name, value=value)
+
+        chosen_index = await numeric_emoji_selector(ctx, len(possible_matches), 30, embed=embed)
+        if chosen_index is None:
+            return None
+        else:
+            return possible_matches[chosen_index][0]
