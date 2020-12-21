e08a3bbcd1678aebc3d7187b1682fc3aea2d6ea7
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2020-12-22 02:16:43 +0530

    Switched to Capitalized Table/Column Names

--- src/dependencies/database/database_initialization.sql
+++ src/dependencies/database/database_initialization.sql
@@ -8,39 +8,39 @@
 
 
 /*  TABLE:  PERMISSIONS_NAMES    */
-CREATE TABLE IF NOT EXISTS USER_AUTH
+CREATE TABLE IF NOT EXISTS "USER_AUTH"
 (
-    ENTRY_ID  SERIAL primary key,
-    ITEM_ID   bigint             not null,
-    LEVEL     int      default 0 not null,
-    ROLE      smallint default 0 not null,
-    SERVER_ID bigint   default 0 not null,
+    "ENTRY_ID"  SERIAL primary key,
+    "ITEM_ID"   bigint             not null,
+    "LEVEL"     int      default 0 not null,
+    "ROLE"      smallint default 0 not null,
+    "SERVER_ID" bigint   default 0 not null,
 
-    constraint ENTRY_ID_UNIQUE
-        unique (ENTRY_ID),
-    constraint ITEM_ID_UNIQUE
-        unique (ITEM_ID)
+    constraint "ENTRY_ID_UNIQUE"
+        unique ("ENTRY_ID"),
+    constraint "ITEM_ID_UNIQUE"
+        unique ("ITEM_ID")
 );
 
 /*  TABLE:  PERMISSIONS_NAMES    */
-CREATE TABLE IF NOT EXISTS PERMISSIONS_NAMES
+CREATE TABLE IF NOT EXISTS "PERMISSIONS_NAMES"
 (
-    ID    SERIAL primary key,
-    NAME  varchar(45) not null,
-    LEVEL int         not null,
+    "ID"    SERIAL primary key,
+    "NAME"  varchar(45) not null,
+    "LEVEL" int         not null,
 
-    constraint LEVEL_UNIQUE
-        unique (LEVEL)
+    constraint "LEVEL_UNIQUE"
+        unique ("LEVEL")
 );
 
 /*  TABLE:  CHANNEL_AUTH    */
-CREATE TABLE IF NOT EXISTS CHANNEL_AUTH
+CREATE TABLE IF NOT EXISTS "CHANNEL_AUTH"
 (
-    ENTRY_ID        SERIAL primary key,
-    SERVER_ID       bigint   not null,
-    CHANNEL_ID      bigint   not null,
-    WHITELIST_LEVEL smallint not null,
+    "ENTRY_ID"        SERIAL primary key,
+    "SERVER_ID"       bigint   not null,
+    "CHANNEL_ID"      bigint   not null,
+    "WHITELIST_LEVEL" smallint not null,
 
-    constraint CHANNEL_ID_UNIQUE
-        unique (CHANNEL_ID)
+    constraint "CHANNEL_ID_UNIQUE"
+        unique ("CHANNEL_ID")
 );
