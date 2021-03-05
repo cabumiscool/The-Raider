d19a870ab5d8cdcc23bc1d4d980c002c4080ae98
cabumiscool <jdraudales1@gmail.com>
2021-03-04 20:03:22 -0600

    fixed small errors in how the buyer pool worked

--- src/background_process/buyer_service.py
+++ src/background_process/buyer_service.py
@@ -1,12 +1,16 @@
 import asyncio
 import aiohttp
 import typing
+import time
 from background_process.base_service import BaseService
 from dependencies.database.database import Database
 from dependencies.webnovel import classes
 from dependencies.proxy_classes import Proxy
 from dependencies.webnovel.web import book
 from dependencies.webnovel.waka import book as wbook
+
+
+default_connector_settings = {'force_close': True, 'enable_cleanup_closed': True}
 
 
 class BuyManager:
@@ -25,22 +29,24 @@ class BuyManager:
         return bool(self._task.exception())
 
 
-# TODO Finish writing the handler in case of error
+# TODO Finish writing the handler in case of error | could be done.... need to check in detail
 class BuyerPool:
     def __init__(self, account: classes.QiAccount, proxy: Proxy = None):
         self._proxy = proxy
         self._account = account
         self._slots = account.fast_pass_count
         if self._proxy:
-            self._connector = proxy.generate_connector()
+            self._connector = proxy.generate_connector(**default_connector_settings)
         else:
-            self._connector = aiohttp.TCPConnector()
+            self._connector = aiohttp.TCPConnector(**default_connector_settings)
         self._buys = []
         self._completed_chapters = []
         self._completed_buys = []
         self._uncompleted_chapters_to_return = []
+        self._created_time = time.time()
 
-        # TODO find if there is a non deprecated form
+        # TODO find if there is a non deprecated form, ps. the deprecated form is creating it from an sync code,
+        #  creating it from an async code is legal
         self._session = aiohttp.ClientSession(connector=self._connector, cookies=account.cookies)
 
     def __return__completed_chapter(self) -> list:
@@ -52,6 +58,8 @@ class BuyerPool:
         return return_list
 
     def available_capacity(self) -> bool:
+        if time.time() - self._created_time >= 180:
+            self._slots = 0
         return self._slots > 0
 
     def is_empty(self) -> bool:
@@ -64,7 +72,7 @@ class BuyerPool:
         return len(self._uncompleted_chapters_to_return) > 0
 
     def return_uncompleted_chapters(self):
-        return self._completed_chapters
+        return self._uncompleted_chapters_to_return
 
     async def retrieve_done(self) -> list:
         for buy_manager in self._buys:
@@ -77,6 +85,7 @@ class BuyerPool:
                                 self._slots = self._account.fast_pass_count
 
                             self._buys.append(BuyManager(buy_manager.chapter, self._session))
+                            self._buys.remove(buy_manager)
                             self._slots -= 1
                         else:
                             self._slots = 0
@@ -94,6 +103,12 @@ class BuyerPool:
     def buy(self, chapter: classes.SimpleChapter):
         self._buys.append(BuyManager(chapter, self._session))
         self._slots -= 1
+
+    def return_account(self):
+        return self._account
+
+    async def close(self):
+        await self._session.close()
 
 
 class WakaBuyManager:
@@ -119,7 +134,7 @@ class WakaBuyerPool:
         self._buyers = []
         self._done_managers = []
         self._chapters = []
-        self._connector = waka_proxy.generate_connector()
+        self._connector = waka_proxy.generate_connector(**default_connector_settings)
         self._session = aiohttp.ClientSession(connector=self._connector)
 
     def retrieve_done(self) -> list:
@@ -147,7 +162,7 @@ class WakaBuyerPool:
 
 class BuyerService(BaseService):
     def __init__(self, database: Database):
-        super().__init__("Buyer Service Module")
+        super().__init__("Buyer Service")
         self.database = database
         self.pools = []
         self.priv_buyer = None
@@ -164,7 +179,7 @@ class BuyerService(BaseService):
                 self.priv_buyer.buy(chapter)
             else:
                 for pool in self.pools:
-                    pool: typing.Union[BuyerPool, WakaBuyerPool]
+                    pool: typing.Union[BuyerPool]
                     if pool.available_capacity():
                         pool.buy(chapter)
                         break
@@ -173,15 +188,21 @@ class BuyerService(BaseService):
                     while True:
                         account_working = await account.async_check_valid()
                         if account_working:
+                            if account.fast_pass_count == 0:
+                                await self.database.update_account_fp_count(0, account)
+                                continue
                             break
                         else:
                             await self.database.expired_account(account)
                             account = await self.database.retrieve_buyer_account()
-                    self.pools.append(BuyerPool(account=account))
+                    new_pool = BuyerPool(account=account)
+                    new_pool.buy(chapter)
+                    self.pools.append(new_pool)
 
         # will add to the output cache the completed values from the pools
         for pool in self.pools:
-            self._output_queue.append(pool.retrieve_done())
+            self._output_queue.extend(await pool.retrieve_done())
+        self._output_queue.extend(self.priv_buyer.retrieve_done())
 
         # will look for empty pools and clean up
         pool_to_delete = []
@@ -195,4 +216,11 @@ class BuyerService(BaseService):
                     pool_to_delete.append(pool)
 
         for pool in pool_to_delete:
+            pool_account = pool.return_account()
+            await pool_account.async_check_valid()
+            await self.database.update_account_fp_count(pool_account.fast_pass_count, pool_account)
+            await self.database.release_account(pool_account)
+            await pool.close()
             self.pools.remove(pool)
+
+        await self.database.release_accounts_over_five_in_use_minutes()
