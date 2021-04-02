711132a7bb1b635a25a0dab29fd51531fe9b8d83
cabumiscool <jdraudales1@gmail.com>
2021-04-01 22:29:17 -0600

    Optimized how pastes were created to prevent the task from blocking itself

--- src/background_process/paste_service.py
+++ src/background_process/paste_service.py
@@ -6,6 +6,7 @@ import privatebinapi
 from background_process.base_service import BaseService
 from background_process import background_objects
 from dependencies.webnovel import classes
+from dependencies.webnovel.web.book import full_book_retriever
 
 
 paste_metadata = '<h3 data-book-Id="%s" data-chapter-Id="%s" data-almost-unix="%s" ' \
@@ -16,7 +17,7 @@ data_from = ['qi', 'waka-waka']
 
 class Paste:
     def __init__(self, paste_id: str, paste_full_url: str, paste_delete_token: str, paste_passcode: str, status: int,
-                 simple_book: classes.SimpleBook, chapters_id: typing.List[int], ranges: typing.Tuple[int, int]):
+                 book: classes.Book, chapters_id: typing.List[int], ranges: typing.Tuple[int, int]):
         if type(paste_id) != str:
             self.id = str(paste_id)
         else:
@@ -25,7 +26,7 @@ class Paste:
         self.delete_token = paste_delete_token
         self.passcode = paste_passcode
         self.status_code = status
-        self.book_obj = simple_book
+        self.book_obj = book
         self.chapters_ids = chapters_id
         self.ranges = ranges
 
@@ -62,13 +63,14 @@ class PasteRequest:
 async def paste_builder(paste_request: typing.Union[PasteRequest, MultiPasteRequest]):
     paste_dict = await privatebinapi.send_async('https://vim.cx/', text=paste_request.return_paste_content(),
                                                 formatting='markdown')
+    complete_book = await full_book_retriever(paste_request.book)
     if isinstance(paste_request, PasteRequest):
         return Paste(paste_dict['id'], paste_dict['full_url'], paste_dict['deletetoken'], paste_dict['passcode'],
-                     paste_dict['status'], paste_request.book, [paste_request.chapter.id],
+                     paste_dict['status'], complete_book, [paste_request.chapter.id],
                      (paste_request.chapter.index, paste_request.chapter.index))
     elif isinstance(paste_request, MultiPasteRequest):
         return Paste(paste_dict['id'], paste_dict['full_url'], paste_dict['deletetoken'], paste_dict['passcode'],
-                     paste_dict['status'], paste_request.book, [chapter.id for chapter in paste_request.chapters],
+                     paste_dict['status'], complete_book, [chapter.id for chapter in paste_request.chapters],
                      (paste_request.chapters[0].index, paste_request.chapters[-1].index))
     else:
         # should an error be raised here in case an unknown object is passed?
@@ -78,22 +80,35 @@ async def paste_builder(paste_request: typing.Union[PasteRequest, MultiPasteRequ
 class PasteCreator(BaseService):
     def __init__(self):
         super().__init__('Paste Creator Service', loop_time=5)
+        self.pastes_tasks = []
 
     async def main(self):
-        pastes = []
+        completed_tasks = []
         exceptions = []
         input_content = self._retrieve_input_queue()
         input_content: typing.List[typing.Union[PasteRequest, MultiPasteRequest]]
         for item in input_content:
-            pastes.append(asyncio.create_task(paste_builder(item)))
+            self.pastes_tasks.append(asyncio.create_task(paste_builder(item)))
 
         # maybe write a wrapper here to catch the exceptions and retry
-        results = await asyncio.gather(*pastes, return_exceptions=True)
-        for paste in results:
-            if issubclass(type(paste), Exception):
-                exceptions.append(paste)
-            else:
-                self._output_queue.append(paste)
+        # results = await asyncio.gather(*pastes, return_exceptions=True)
+        for paste_task in self.pastes_tasks:
+            paste_task: asyncio.Task
+            if paste_task.done():
+                if paste_task.cancelled():
+                    completed_tasks.append(paste_task)
+                else:
+                    try:
+                        self._output_queue.append(paste_task.result())
+                        completed_tasks.append(paste_task)
+                    except asyncio.CancelledError as e:
+                        raise e
+                    except Exception as e:
+                        exceptions.append(e)
+                        completed_tasks.append(paste_task)
+
+        for completed_task in completed_tasks:
+            self.pastes_tasks.remove(completed_task)
 
         if len(exceptions) > 1:
             raise background_objects.ErrorList(*exceptions)
