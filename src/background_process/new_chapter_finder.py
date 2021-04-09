703e21fd9687279de21f67dc8f5cac301872a7a7
cabumiscool <cabumiscool@github.com>
2021-04-08 21:32:10 -0600

    made the service more efficient and non blocking

--- src/background_process/new_chapter_finder.py
+++ src/background_process/new_chapter_finder.py
@@ -9,8 +9,21 @@ from dependencies.webnovel.web import book
 
 class NewChapterFinder(BaseService):
     def __init__(self, database: Database):
-        super().__init__(name='Updated Chapter Finder Service')
+        super().__init__(name='Updated Chapter Finder Service', loop_time=10)
         self.database = database
+        self.retrieving_books_tasks: typing.List[typing.Tuple[asyncio.Task, classes.SimpleBook]] = []
+
+    async def compare_qi_book_to_db_book(self, book_obj: classes.SimpleBook):
+        chapter_objs = []
+        complete_qi_book = await book.full_book_retriever(book_obj)
+        db_book = await self.database.retrieve_complete_book(book_id=book_obj.id)
+
+        new_ids = (complete_qi_book != db_book)[0]
+
+        for chapter_id in new_ids:
+            chapter_objs.append(complete_qi_book.retrieve_chapter_by_id(chapter_id))
+
+        return chapter_objs
 
     async def main(self):
         cache_content = self._retrieve_input_queue()
@@ -26,15 +39,24 @@ class NewChapterFinder(BaseService):
         #         working_proxy = await self.database.retrieve_proxy()
 
         for updated_book in cache_content:
-            chapter_objs = []
+            # full_book = await book.full_book_retriever(updated_book)
+            self.retrieving_books_tasks.append((asyncio.create_task(self.compare_qi_book_to_db_book(updated_book)),
+                                                updated_book))
 
-            full_book = await book.full_book_retriever(updated_book)
+        finished_tasks = []
 
-            database_book = await self.database.retrieve_complete_book(updated_book.id)
+        for book_task_tuple in self.retrieving_books_tasks:
+            tuple_book = book_task_tuple[1]
+            tuple_task = book_task_tuple[0]
+            if tuple_task.done():
+                if tuple_task.exception() is None:
+                    chapter_objs = tuple_task.result()
+                    self._output_queue.extend(chapter_objs)
 
-            new_ids = (full_book != database_book)[0]
+                else:
+                    finished_tasks.append(book_task_tuple)
+                    self.retrieving_books_tasks.append((asyncio.create_task(self.compare_qi_book_to_db_book(tuple_book))
+                                                        , tuple_book))
 
-            for chapter_id in new_ids:
-                chapter_objs.append(full_book.retrieve_chapter_by_id(chapter_id))
-
-            self._output_queue.extend(chapter_objs)
+        for finished_task in finished_tasks:
+            self.retrieving_books_tasks.remove(finished_task)
