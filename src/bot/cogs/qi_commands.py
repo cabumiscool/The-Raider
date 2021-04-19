1dae79f31b4b3c53d918b4aa9c55247d69e469ed
Raz Crimson <52282402+RazCrimson3@users.noreply.github.com>
2021-04-20 03:33:08 +0530

    Moving buy_wrapper to dependencies.utils.py as generic_buyer

--- src/bot/cogs/qi_commands.py
+++ src/bot/cogs/qi_commands.py
@@ -1,7 +1,5 @@
 import asyncio
 import re
-import time
-from operator import attrgetter
 from typing import Union, List, Tuple, Dict
 
 import privatebinapi
@@ -9,13 +7,11 @@ from discord.ext import commands
 from discord.ext.commands import Context
 
 from bot.bot_utils import generate_embed, emoji_selection_detector
-from dependencies.database import database_exceptions
-from dependencies.database.database import Database
-from dependencies.privatebin import upload_to_privatebin
-from dependencies.webnovel.classes import SimpleBook, SimpleChapter
+from dependencies.database import database_exceptions, Database
+from dependencies.webnovel.classes import SimpleBook
 from dependencies.webnovel.utils import book_string_to_book_id
-from dependencies.webnovel.waka import book as waka_book
-from dependencies.webnovel.web import book
+from dependencies.webnovel.web.book import full_book_retriever
+from dependencies.utils import generic_buyer
 from . import bot_checks
 
 NUMERIC_EMOTES = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '0⃣']
@@ -24,11 +20,6 @@ range_match = re.compile(r'\[?\**`?(\d+)[ \-]*(\d*)`?\**]?,? ?')
 bloat_content_match = re.compile(r'((:sparkles: )?\**\d+\** chapter[s]? missing from )')
 title_range_match = re.compile(
     r'[`"\' ]?([\w\d,!.:()’?\-\' ]+?)[\'"` ]? ?[\s\- ]+((?:\[?\**`?\d+[ \-]*\d*`?\**]?,? ?)+)\n')
-
-paste_metadata = '<h3 data-book-Id="%s" data-chapter-Id="%s" data-almost-unix="%s" ' \
-                 'data-SS-Price="%s" data-index="%s" data-is-Vip="%s" data-source="qi_latest" data-from="%s" ' \
-                 '>Chapter %s:  %s</h3>'
-data_from = ['qi', 'waka-waka']
 
 
 def book_string_and_range_matcher(user_string: str) -> Dict[str, List[Tuple[int, int]]]:
@@ -80,55 +71,6 @@ class QiCommands(commands.Cog):
             return None
         return await self.db.retrieve_simple_book(possible_matches[NUMERIC_EMOTES.index(chosen_emote)][0])
 
-    async def buy_wrapper(self, book_: SimpleBook, *chapters: SimpleChapter):
-        waka_proxy = await self.db.retrieve_proxy(1)
-
-        # TODO move somewhere else
-        async def individual_buyer(chapter: SimpleChapter):
-            buyer_account = await self.db.retrieve_buyer_account()
-            while True:
-                working_account = await buyer_account.async_check_valid()
-                if working_account:
-                    break
-                else:
-                    buyer_account = await self.db.retrieve_buyer_account()
-
-            if chapter.is_privilege:
-                chapter_obj = await waka_book.chapter_retriever(book_id=chapter.parent_id, chapter_id=chapter.id,
-                                                                volume_index=chapter.volume_index, proxy=waka_proxy)
-            else:
-                chapter_obj = await book.chapter_buyer(book_id=chapter.parent_id, chapter_id=chapter.id,
-                                                       account=buyer_account)
-
-            await self.db.release_account(buyer_account)
-
-            return chapter_obj
-
-        async_tasks = [asyncio.create_task(individual_buyer(chapter)) for chapter in chapters]
-        ranges = [chapter.index for chapter in chapters]
-        ranges.sort()
-        chapters = await asyncio.gather(*async_tasks)
-        chapters.sort(key=attrgetter('index'))
-        chapters_strings = []
-        for chapter in chapters:
-            metadata = paste_metadata % (
-                chapter.parent_id, chapter.id, time.time(), chapter.price, chapter.index,
-                chapter.is_vip, data_from[chapter.is_privilege], chapter.index, chapter.name)
-            chapters_strings.append('\n'.join((metadata, chapter.content)))
-
-        complete_string = '\n'.join(chapters_strings)
-
-        paste_response = await upload_to_privatebin(complete_string)
-        # paste_response = await privatebinapi.send_async(server='https://vim.cx/', text=complete_string,
-        #                                                 formatting="markdown")
-        if ranges[0] == ranges[-1]:
-            range_str = f'{ranges[0]}'
-        else:
-            range_str = f'{ranges[0]}-{ranges[-1]}'
-        paste_url = f"!paste {book_.name} - {range_str} <{paste_response}>"
-
-        return paste_url
-
     @commands.command(aliases=['b'])
     @bot_checks.is_whitelist()
     @bot_checks.check_permission_level(2)
@@ -157,7 +99,7 @@ class QiCommands(commands.Cog):
 
         async_tasks = []
         for book_id, chapters_list in book_chapter_requests.items():
-            async_tasks.append(asyncio.create_task(self.buy_wrapper(books_objs[book_id], *chapters_list)))
+            async_tasks.append(asyncio.create_task(generic_buyer(self.db, books_objs[book_id], *chapters_list)))
 
         pastes = await asyncio.gather(*async_tasks)
         paste_tasks = [asyncio.create_task(ctx.send(paste)) for paste in pastes]
@@ -167,9 +109,9 @@ class QiCommands(commands.Cog):
     @bot_checks.is_whitelist()
     @bot_checks.check_permission_level(2)
     async def id_buy(self, ctx: Context, book_id: int, starting_index: int):
-        book_obj = await book.full_book_retriever(book_id)
+        book_obj = await full_book_retriever(book_id)
         chapter_obj = book_obj.retrieve_chapter_by_index(starting_index)
-        paste = await self.buy_wrapper(book_obj, chapter_obj)
+        paste = await generic_buyer(self.db, book_obj, chapter_obj)
         await ctx.send(paste)
 
     @commands.command(aliases=['bl'])
---
+++ src/dependencies/utils.py
@@ -0,0 +1,62 @@
+import asyncio
+import time
+from operator import attrgetter
+
+from .webnovel.classes import SimpleBook, SimpleChapter
+from .webnovel.waka import book as waka_book
+from .webnovel.web import book
+from .database import Database
+from .privatebin import upload_to_privatebin
+
+paste_metadata = '<h3 data-book-Id="%s" data-chapter-Id="%s" data-almost-unix="%s" ' \
+                 'data-SS-Price="%s" data-index="%s" data-is-Vip="%s" data-source="qi_latest" data-from="%s" ' \
+                 '>Chapter %s:  %s</h3>'
+data_from = ['qi', 'waka-waka']
+
+
+async def generic_buyer(db: Database, book_: SimpleBook, *chapters: SimpleChapter):
+    waka_proxy = await db.retrieve_proxy(1)
+
+    async def individual_buyer(chapter: SimpleChapter):
+        buyer_account = await db.retrieve_buyer_account()
+        while True:
+            working = await buyer_account.async_check_valid()
+            if working:
+                break
+            buyer_account = await db.retrieve_buyer_account()
+
+        if chapter.is_privilege:
+            chapter_obj = await waka_book.chapter_retriever(book_id=chapter.parent_id, chapter_id=chapter.id,
+                                                            volume_index=chapter.volume_index, proxy=waka_proxy)
+        else:
+            chapter_obj = await book.chapter_buyer(book_id=chapter.parent_id, chapter_id=chapter.id,
+                                                   account=buyer_account)
+
+        await db.release_account(buyer_account)
+
+        return chapter_obj
+
+    async_tasks = [asyncio.create_task(individual_buyer(chapter)) for chapter in chapters]
+    ranges = [chapter.index for chapter in chapters]
+    ranges.sort()
+    chapters = await asyncio.gather(*async_tasks)
+    chapters.sort(key=attrgetter('index'))
+    chapters_strings = []
+    for chapter in chapters:
+        metadata = paste_metadata % (
+            chapter.parent_id, chapter.id, time.time(), chapter.price, chapter.index,
+            chapter.is_vip, data_from[chapter.is_privilege], chapter.index, chapter.name)
+        chapters_strings.append('\n'.join((metadata, chapter.content)))
+
+    complete_string = '\n'.join(chapters_strings)
+
+    paste_response = await upload_to_privatebin(complete_string)
+    # paste_response = await privatebinapi.send_async(server='https://vim.cx/', text=complete_string,
+    #                                                 formatting="markdown")
+    if ranges[0] == ranges[-1]:
+        range_str = f'{ranges[0]}'
+    else:
+        range_str = f'{ranges[0]}-{ranges[-1]}'
+    paste_url = f"!paste {book_.name} - {range_str} <{paste_response}>"
+
+    return paste_url
