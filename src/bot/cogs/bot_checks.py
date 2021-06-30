ea7c7208862cfe5ca8ee104cee8e9adf32d854f4
cabumiscool <cabumiscool@github.com>
2021-06-29 22:04:45 -0600

    small budfixes for the bot checks when used in the dm

--- src/bot/cogs/bot_checks.py
+++ src/bot/cogs/bot_checks.py
@@ -28,7 +28,10 @@ def check_permission_level(required_level: int = 0):
         db: Database = bot.db
         author: discord.Member = ctx.author
         is_god: bool = await ctx.bot.is_owner(ctx.author) or ctx.author.id in [479487273432514560, 339050854453608459]
-        ids = [author.id, *[role.id for role in author.roles]]
+        if hasattr(author, "roles"):
+            ids = [author.id, *[role.id for role in author.roles]]
+        else:
+            ids = [author.id]
         perm = await db.permission_retriever(*ids)
         if perm is None:
             perm = 0
@@ -43,7 +46,10 @@ def is_whitelist():
     async def check(ctx: Context):
         db: Database = ctx.bot.db
         channel_id: int = ctx.channel.id
-        server_id: int = ctx.guild.id
+        if hasattr(ctx.guild, 'id'):
+            server_id: int = ctx.guild.id
+        else:
+            server_id = 0
         check_ = await db.whitelist_check(server_id, channel_id)
         if check_:
             return check_
