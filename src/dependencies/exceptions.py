409c00301c4e757bd88f3c5841c21b774fdec2f7
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2021-07-17 01:47:24 +0530

    Exception - Add: Error Code map

--- src/dependencies/exceptions.py
+++ src/dependencies/exceptions.py
@@ -7,3 +7,9 @@ class RaiderBaseException(Exception):
 
     def get_message(self) -> str:
         return self.MESSAGE
+
+# Error code map:
+# 100 : Config Exceptions - config.py
+# 200 : Webnovel Exceptions - dependencies/webnovel
+# 300 : Mail Agent Exceptions - dependencies/mail_agent
+# 900 : Daemon Exceptions - background_process/background_objections.py
