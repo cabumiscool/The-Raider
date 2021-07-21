602894ac99ee01362915093f806c2adc451bcafb
RazCrimson <52282402+RazCrimson@users.noreply.github.com>
2021-07-22 01:28:51 +0530

    Refactor cookie_maintainer_service.py

--- src/background_process/services/cookie_maintainer_service.py
+++ src/background_process/services/cookie_maintainer_service.py
@@ -16,62 +16,58 @@ class CookieMaintainerService(BaseService):
         self.captcha_block = 0
 
     async def main(self):
-        expired_account = await self.db.retrieve_expired_account()
-        if expired_account is None:
+        expired_acc = await self.db.retrieve_expired_account()
+        if expired_acc is None:
             return
 
-        host_index = expired_account.host_email_id
-
-        email_account = await self.db.retrieve_email_obj(id_=host_index)
-        mail_agent = MailAgent(email_account.email, email_account.password)
-        await mail_agent.initialize()
-
-        async with aiohttp.ClientSession(cookies=expired_account.cookies) as session:
+        async with aiohttp.ClientSession(cookies=expired_acc.cookies) as session:
             try:
-                response, ticket = await auth.check_status(expired_account.ticket, session)
+                response, ticket = await auth.check_status(expired_acc.ticket, session)
 
                 if response['code'] != 0:
-                    response, ticket = await auth.check_code(session, ticket, expired_account.email,
-                                                             expired_account.password)
+                    response, ticket = await auth.check_code(session, ticket, expired_acc.email, expired_acc.password)
 
-                    if response['code'] == 11318:
-                        encry_param = response['encry']
-                        response = await auth.send_trust_email(session, ticket, encry_param)
-                        await asyncio.sleep(55)
-                        keycode = await mail_agent.get_keycode_by_recipient(expired_account.email)
+                if response['code'] == 11318:
+                    host_index = expired_acc.host_email_id
+                    email_account = await self.db.retrieve_email_obj(id_=host_index)
+                    mail_agent = MailAgent(email_account.email, email_account.password)
+                    await mail_agent.initialize()
 
-                        response, ticket = await auth.check_trust(session, ticket, encry_param, keycode)
-                        expired_account.ticket = ticket
+                    encry_param = response['encry']
+                    response = await auth.send_trust_email(session, ticket, encry_param)
+                    await asyncio.sleep(45)
+                    keycode = await mail_agent.get_keycode_by_recipient(expired_acc.email)
 
-                    elif response['code'] == 11401:
-                        print('Captcha block!')
-                        self.captcha_block = time.time()
-                        raise Exception(f"Captcha blocked at {int(time.time())}")
+                    response, ticket = await auth.check_trust(session, ticket, encry_param, keycode)
+
+                if response['code'] == 11401:
+                    print('Captcha block!')
+                    self.captcha_block = time.time()
+                    raise Exception(f"Captcha blocked at {int(time.time())}")
 
                 response_code = response['code']
+                cookies_dict = {}
+                for cookie in session.cookie_jar:
+                    cookie_key = cookie.key
+                    cookie_value = cookie.value
+                    cookies_dict[cookie_key] = cookie_value
+
+                expired_acc.cookies = cookies_dict
+                expired_acc.ticket = ticket
                 if response_code == 0:
-                    expired_account.expired = False
-                    update_db_flg = True
+                    expired_acc.expired = False
 
-                elif response_code == -51018:
-                    # Invoke ticket error. Updating Cookies and redoing request seems to clear it
-                    update_db_flg = True
+                await self.db.update_account_params(expired_acc)
 
-                else:
-                    raise Exception(f'Unknown Response for {expired_account.email}! Response_code: {response["code"]}.'
+                # Send it to the log channel
+                if response_code not in [0, -51018]:
+                    # 11104: Internet error. The returned cookies actually seem to be valid for check_trust
+                    # -51018: Invoke ticket error. Updating Cookies and redoing request seems to clear it
+
+                    raise Exception(f'Unknown Response for {expired_acc.email}! Response_code: {response["code"]}.'
                                     f'\nResponse:{response}')
 
-                if update_db_flg is True:
-                    cookies_dict = {}
-                    for cookie in session.cookie_jar:
-                        cookie_key = cookie.key
-                        cookie_value = cookie.value
-                        cookies_dict[cookie_key] = cookie_value
-                    expired_account.cookies = cookies_dict
-                    await self.db.update_account_params(expired_account)
-
             except Exception as e:
-                await self.db.update_account_params(expired_account)
                 raise e
 
     async def inner_loop_manager(self):
